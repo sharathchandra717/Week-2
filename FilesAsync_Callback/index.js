aea1a7bd7af6ce944944216a6e0d9f347653cb79
Sharath Chandra <31856935+sharathchandra717@users.noreply.github.com>
2018-11-15 21:07:36 +0530

    Update index.js

--- FilesAsync_Callback/index.js
+++ FilesAsync_Callback/index.js
@@ -1,5 +1,5 @@
 function copy() {
-    let fs = require('fs'); 
+    const fs = require('fs'); 
     for (let i = 1; i <= 10; i++) {
         fs.readFile("readMe"+i+".txt","utf8",function(err,data) {
             if (err) throw err;
