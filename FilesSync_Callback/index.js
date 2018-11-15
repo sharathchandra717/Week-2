13574ee5c78ca5c44cfa29b2fa245f2920149d8e
Sharath Chandra <31856935+sharathchandra717@users.noreply.github.com>
2018-11-15 20:52:47 +0530

    Update index.js

--- FilesSync_Callback/index.js
+++ FilesSync_Callback/index.js
@@ -3,20 +3,9 @@ function copy() {
     for (let i = 1; i <= 10; i++) {
         data = fs.readFileSync("readMe"+i+".txt","utf8");
         fs.appendFileSync("writeMe.txt",data+"\n");
-        console.log("Copied readme"+i);
     }      
 }
 
 copy();
 
-/*Output:
-Copied readme1
-Copied readme2
-Copied readme3
-Copied readme4
-Copied readme5
-Copied readme6
-Copied readme7
-Copied readme8
-Copied readme9
-Copied readme10*/
+//Output:(in writeMe.txt)
