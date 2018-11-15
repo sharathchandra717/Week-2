e7637738a10167a1c1bf2a1c49032f9106f69be3
Sharath Chandra <31856935+sharathchandra717@users.noreply.github.com>
2018-11-15 21:08:03 +0530

    Update index.js

--- FilesSync_Callback/index.js
+++ FilesSync_Callback/index.js
@@ -1,5 +1,5 @@
 function copy() {
-    let fs = require('fs'),data; 
+    const fs = require('fs'),data; 
     for (let i = 1; i <= 10; i++) {
         data = fs.readFileSync("readMe"+i+".txt","utf8");
         fs.appendFileSync("writeMe.txt",data+"\n");
